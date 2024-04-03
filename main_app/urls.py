d15fdc0a9717126995f6a13f3f9fa8c7fd839139
JimHaff <james.haff@generalassemb.ly>
2024-04-03 13:26:02 -0500

    add base.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	# django's convention is to use a trailing / for 
+	# the routE
 	path('about/', views.about, name='about'),
 ]
